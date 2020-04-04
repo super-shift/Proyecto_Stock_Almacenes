4d3ab371f7fa7f965cfeffbc21144efa015aa578
jandobull <jandobull@gmail.com>
2020-04-04 17:21:37 -0300

    Nuevo Cambio

--- tabla_precios.js
+++ tabla_precios.js
@@ -6,11 +6,11 @@ var valor_4 = 5;
 var valor_5 = 7;
 
 //// por completar
-var frase_1 += ((valor_1).toString() + " monedas");
-var frase_2 += ((valor_2).toString() + " moneda");
-var frase_3 += ((valor_3).toString() + " monedas");
-var frase_4 += ((valor_4).toString() + " monedas");
-var frase_5 += ((valor_5).toString() + " monedas");
+//var frase_1 += ((valor_1).toString() + " monedas");
+//var frase_2 += ((valor_2).toString() + " moneda");
+//var frase_3 += ((valor_3).toString() + " monedas");
+//var frase_4 += ((valor_4).toString() + " monedas");
+//var frase_5 += ((valor_5).toString() + " monedas");
 ////
 
 // poner precios 
